aee5fdc7858322ac9512ea537ee025bd67ffecb1
Jordan Dains <dains.jordan2@gmail.com>
2020-09-29 14:39:28 +0000

    Automatically backed up by Learn

--- spec/guessing_cli_spec.rb
+++ spec/guessing_cli_spec.rb
@@ -11,9 +11,9 @@ describe "Guessing CLI" do
     it "responds to an incorrect guess" do
       allow(self).to receive(:rand).and_return(2)
 
-      expect(self).to receive(:gets).and_return("5")
+      expect(self).to receive(:gets).and_return("2")
 
-      expect { run_guessing_game }.to output(/Sorry! The computer guessed 6./).to_stdout
+      expect { run_guessing_game }.to output(/Sorry! The computer guessed 5./).to_stdout
     end
 
     it "responds to a correct guess" do
